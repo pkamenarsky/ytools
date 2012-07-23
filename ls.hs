4386e67c9cb3348f27b4320c9155be33e8fe8fe9
Philip Kamenarsky <pk@harmonious.at>
2012-07-23 17:13:33 +0200

    file permissions

--- ls.hs
+++ ls.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Ls where
 
 import qualified Control.Exception as E
@@ -7,7 +9,21 @@ import Control.Monad
 import System.Posix.Directory
 import System.Posix.Files
 
+import Data.Generics
 import Data.List
+
+import qualified Text.JSON as J
+
+--
+
+toString = J.JSString . J.toJSString
+
+withDirStream = E.bracket (openDirStream ".") closeDirStream
+
+catchAll :: E.SomeException -> IO ()
+catchAll = \e -> return ()
+
+--
 
 class PrintableC a where
 	printC :: a -> String
@@ -22,13 +38,27 @@ instance PrintableC FileStatus where
 instance PrintableC FileInfo where
 	printC info = path info ++ " " ++ printC (status info)
 
+instance J.JSON FileInfo where
+	readJSON json = undefined
+	showJSON info = let
+		mode = fileMode (status info)
+		checkMode = J.JSBool . (/= nullFileMode) . intersectFileModes mode
+		in J.makeObj
+			[("path", toString $ path info),
+			("permissions", J.makeObj
+				[("uwrite", checkMode ownerWriteMode),
+				("uread", checkMode ownerReadMode),
+				("uexec", checkMode ownerExecuteMode),
+				("gwrite", checkMode groupWriteMode),
+				("gread", checkMode groupReadMode),
+				("gexec", checkMode groupExecuteMode),
+				("owrite", checkMode otherWriteMode),
+				("oread", checkMode otherReadMode),
+				("oexec", checkMode otherExecuteMode)])]
+
 getFileInfo :: FilePath -> IO FileInfo
 getFileInfo path = FileInfo <$> return path <*> getFileStatus path
 
-withDirStream = E.bracket (openDirStream ".") closeDirStream
-
-main = catch 
-	(withDirStream (\stream -> forever $ do
-		info <- readDirStream stream >>= getFileInfo
-		putStrLn $ printC info))
-	(\e -> return ())
+main = E.handle catchAll $ withDirStream $ \stream -> forever $ do
+	info <- readDirStream stream >>= getFileInfo
+	putStrLn $ J.encodeStrict $ J.showJSON info
