0dd0ad3eec02bfd7c614a62bab7b1f032e5a9bca
Philip Kamenarsky <pk@harmonious.at>
2012-07-24 22:52:09 +0200

    expressions

--- parse.hs
+++ parse.hs
@@ -10,12 +10,14 @@ import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Combinator
 import Text.ParserCombinators.Parsec.Char
 
-data Select a = Select String String deriving Show
+import Text.JSON
 
-data Expr = And Expr Expr | Or Expr Expr | Token String deriving Show
+-- Expressions
+
+data Expr = And Expr Expr | Or Expr Expr | Equal String String | Greater String String | Less String String deriving Show
 
 identifier :: Parser String
-identifier = try $ many1 $ letter <|> char '.'
+identifier = try $ many1 $ alphaNum <|> char '.'
 
 op :: String -> Parser String
 op str = try $ do
@@ -25,10 +27,14 @@ op str = try $ do
 	return s
 
 expr :: Parser Expr
-expr = chainl1 parseToken (parseAnd <|> parseOr)
+expr = chainl1 (parseEq Equal "=" <|> parseEq Greater ">" <|> parseEq Less "<") (parseAnd <|> parseOr)
 
-parseToken :: Parser Expr
-parseToken = Token <$> identifier
+parseEq :: (String -> String -> Expr) -> String -> Parser Expr
+parseEq ctr opstr = try $ do
+	e1 <- identifier
+	op opstr
+	e2 <- identifier
+	return $ ctr e1 e2
 
 parseAnd :: Parser (Expr -> Expr -> Expr)
 parseAnd = op "&&" >> return And
@@ -36,24 +42,29 @@ parseAnd = op "&&" >> return And
 parseOr :: Parser (Expr -> Expr -> Expr)
 parseOr = op "||" >> return Or
 
-parseSelect :: Parser (Select String)
-parseSelect = do
-	string "select "
-	field <- identifier
-	string " from "
-	source <- identifier
-	return $ Select field source
+eval :: JSValue -> Expr -> Bool
+eval (JSObject obj) (Equal e1 e2) = case valFromObj e1 obj of
+	Ok val -> val == e2
+	_ -> False
+eval obj (And s1 s2) = eval obj s1 && eval obj s2
+eval obj (Or s1 s2) = eval obj s1 || eval obj s2
 
-main = print $ parse expr "source" "aaas && tokenA && tokenB || tokenC && tokenD"
+-- main
 
 parseArgs :: [String] -> IO ()
-parseArgs ["-R"] = print "YES"
-parseArgs ["-U"] = print "NO"
-parseArgs [] = print "NOTHING"
+parseArgs ["-R"] = putStrLn ""
+parseArgs ["-U"] = putStrLn ""
+parseArgs [] = putStrLn ""
 
-main2 = do
+main = do
 	getArgs >>= parseArgs
 	status <- getFdStatus stdOutput
 	if isCharacterDevice status
-		then print "char"
-		else print "notchar"
+		then putStrLn ""
+		else putStrLn ""
+	let parsed = parse expr "source" "path = cool && size = 456"
+	case parsed of
+		Right e -> putStrLn $ show $ eval (makeObj
+			[("path", JSString $ toJSString "cool"),
+			("size", JSString $ toJSString "456")]) e
+		Left e -> putStrLn $ "Parse error: " ++ show e
