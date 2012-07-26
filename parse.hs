e4b248ba5a4e3e9b5e4fbddf855b1798ba363777
Philip Kamenarsky <pk@harmonious.at>
2012-07-26 19:54:12 +0200

    mid refactoring

--- parse.hs
+++ parse.hs
@@ -18,7 +18,14 @@ import Schema
 
 -- Parsing
 
-data Expr = And Expr Expr | Or Expr Expr | Equal LValue RValue | Greater LValue RValue | Less LValue RValue deriving Show
+data LValue = LKeyPath KeyPath | Command String KeyPath deriving (Eq, Show)
+data RValue = RString String | RKeyPath KeyPath deriving (Eq, Show)
+
+data EqOp l v = Equal l v | Greater l v | Less l v deriving (Eq, Show)
+data Expr l v = And (Expr l v) (Expr l v) | Or (Expr l v) (Expr l v) | Op (EqOp l v) deriving (Eq, Show)
+
+foldExpr :: Expr l v -> (EqOp l v -> EqOp l' v') -> Expr l' v'
+foldExpr (And l v) f = undefined
 
 lexer = T.makeTokenParser emptyDef {
 	identLetter = char '.' <|> alphaNum,
@@ -38,16 +45,16 @@ lvalue = LKeyPath <$> (lexeme $ key []) where
 		(char '.' >> key (keys ++ [p])) <|> (return (keys ++ [p]))
 
 rvalue :: Parser RValue
-rvalue = RString <$> identifier <|>
+rvalue = RString <$> lexeme (many1 (oneOf ":.,-" <|> alphaNum)) <|>
 	RString <$> lexeme (between (char '\'') (char '\'') (many1 $ satisfy (/= '\'')))
 
-parseEq :: (LValue -> RValue -> Expr) -> String -> Parser Expr
-parseEq ctr opstr = ctr <$> lvalue <* reservedOp opstr <*> rvalue
+parseEq :: (LValue -> RValue -> (EqOp LValue RValue)) -> String -> Parser (Expr LValue RValue)
+parseEq ctr opstr = Op <$> (ctr <$> lvalue <* reservedOp opstr <*> rvalue)
 
 parseOp :: String -> a -> Parser a
 parseOp str ctr = reservedOp str >> return ctr
 
-expr :: Parser Expr
+expr :: Parser (Expr LValue RValue)
 expr = chainl1 (parens expr <|>
 	(try (parseEq Equal "==") <|>
 		parseEq Equal "=") <|>
@@ -57,30 +64,30 @@ expr = chainl1 (parens expr <|>
 
 -- Evaluation
 
-extractValue :: JSValue -> LValue -> Either String String
-extractValue (JSObject obj) (LKeyPath [key]) = case valFromObj key obj of
-	Ok v -> Right v
-	_ -> Left $ "Extracting value failed: " ++ key
-extractValue (JSObject obj) (LKeyPath (key:keys)) = case valFromObj key obj of
-	Ok obj' -> extractValue obj' (LKeyPath keys)
-	_ -> Left $ "Extracting value failed: " ++ key
-
-eval :: JSValue -> Expr -> (String -> Ordering) -> Either String Bool
-eval obj (Equal e1 e2) cmpFn = case cmpFn <$> extractValue obj e1 of
-	Right ord -> Right $ EQ == ord
-	Left e -> Left e
-eval obj (And s1 s2) cmpFn = (&&) <$> eval obj s1 cmpFn <*> eval obj s2 cmpFn
-eval obj (Or s1 s2) cmpFn = (||) <$> eval obj s1 cmpFn <*> eval obj s2 cmpFn
-
-makeEvalFn :: Schema -> Expr -> (JSValue -> Either String Bool)
-makeEvalFn schema (Equal e1 e2) = let f = cmpFn schema e1 e2 in
-	\value -> case f <$> extractValue value e1 of
-		Right ord -> (==) <$> ord <*> (return EQ)
-		Left e -> Left e
-makeEvalFn schema (And e1 e2) = let
-	f1 = makeEvalFn schema e1
-	f2 = makeEvalFn schema e2 in
-		\value -> (&&) <$> f1 value <*> f2 value
+-- extractValue :: JSValue -> LValue -> Either String String
+-- extractValue (JSObject obj) (LKeyPath [key]) = case valFromObj key obj of
+	-- Ok v -> Right v
+	-- _ -> Left $ "Extracting value failed: " ++ key
+-- extractValue (JSObject obj) (LKeyPath (key:keys)) = case valFromObj key obj of
+	-- Ok obj' -> extractValue obj' (LKeyPath keys)
+	-- _ -> Left $ "Extracting value failed: " ++ key
+-- 
+-- eval :: JSValue -> Expr -> (String -> Ordering) -> Either String Bool
+-- eval obj (Equal e1 e2) cmpFn = case cmpFn <$> extractValue obj e1 of
+	-- Right ord -> Right $ EQ == ord
+	-- Left e -> Left e
+-- eval obj (And s1 s2) cmpFn = (&&) <$> eval obj s1 cmpFn <*> eval obj s2 cmpFn
+-- eval obj (Or s1 s2) cmpFn = (||) <$> eval obj s1 cmpFn <*> eval obj s2 cmpFn
+-- 
+-- makeEvalFn :: Schema -> Expr -> (JSValue -> Either String Bool)
+-- makeEvalFn schema (Equal e1 e2) = let f = cmpFn schema e1 e2 in
+	-- \value -> case f <$> extractValue value e1 of
+		-- Right ord -> (==) <$> ord <*> (return EQ)
+		-- Left e -> Left e
+-- makeEvalFn schema (And e1 e2) = let
+	-- f1 = makeEvalFn schema e1
+	-- f2 = makeEvalFn schema e2 in
+		-- \value -> (&&) <$> f1 value <*> f2 value
 
 -- main
 
@@ -97,11 +104,9 @@ main = do
 		then putStrLn ""
 		else putStrLn ""
 	let pexp = parse expr "" "path = 'cool' && size = 456 && permissions.read = 'true'"
-	case pexp of
-		Right pexp' -> let cmpFn = makeEvalFn lsSchema pexp' in
-			putStrLn $ show $ cmpFn (makeObj
+	let obj = makeObj
 				[("path", JSString $ toJSString "cool"),
 				("size", JSString $ toJSString "456"),
 				("permissions", makeObj
-					[("read", JSString $ toJSString "true")])])
-		Left e -> putStrLn $ "Parse error: " ++ show e
+					[("read", JSString $ toJSString "true")])]
+	print 5
--- schema.hs
+++ schema.hs
@@ -11,9 +11,6 @@ import Text.JSON
 data TypeEnum = IntType | FloatType | BoolType | DateType | StringType deriving (Enum, Show, Eq)
 data Typed a = Typed TypeEnum a
 
-data LValue = LKeyPath KeyPath deriving (Eq, Show)
-data RValue = RString String | RKeyPath KeyPath deriving (Eq, Show)
-
 type KeyPath = [String]
 
 data Schema = Object [(String, Schema)] |
@@ -26,48 +23,30 @@ lsSchema = Object [
 	("permissions", Object [
 		("oread", Field BoolType)])]
 
-lookupKP :: Schema -> KeyPath -> Either String TypeEnum
-lookupKP _ [] = Left "Empty keypath"
-lookupKP (Object fields) [x] = case lookup x fields of
+lookupType :: Schema -> KeyPath -> Either String TypeEnum
+lookupType _ [] = Left "Empty keypath"
+lookupType (Object fields) [x] = case lookup x fields of
 	Just (Field t) -> Right t
 	Just (Object _) -> Left $ "Key " ++ x ++ " is an object"
 	Just (Array _) -> Left $ "Key " ++ x ++ " is an array"
 	_ -> Left $ "No field " ++ x ++ " found in object"
-lookupKP (Object fields) (x:xs) = case lookup x fields of
-	Just s@(Object _) -> lookupKP s xs
+lookupType (Object fields) (x:xs) = case lookup x fields of
+	Just s@(Object _) -> lookupType s xs
 	Just (Field t) -> Left $ "Key " ++ x ++ " is a field but keypath is not exhausted"
 	Just (Array _) -> Left $ "Key " ++ x ++ " is an array"
 	_ -> Left $ "No field " ++ x ++ " found in object"
-lookupKP _ _ = Left $ "Invalid keypath"
+lookupType _ _ = Left $ "Invalid keypath"
 
--- Would be better if cmpFn took an LValue and an RValue and produced a function that
--- extracts the LValue from a js object and then compares it to the RValue (could be a keypath
--- in later versions), i.e: cmpFn :: LValue -> RValue -> (JSObject -> Ordering)
--- cmpFn is going to be useful for ysort, ygroup etc too.
+readMaybe :: (Read a) => String -> String -> Either String a
+readMaybe str msg =  case [x | (x, t) <- reads str, ("", "") <- lex t] of
+	[x] -> Right x
+	_ -> Left $ str ++ " not a " ++ msg
 
-safeRead :: Read a => String -> String -> Either String a
-safeRead str msg = case reads str of
-	[] -> Left $ str ++ " not an " ++ msg
-	(_, y:ys):xs -> Left $ str ++ " not an " ++ msg
-	[(a, [])] -> Right a
+compareJSValues :: JSValue -> JSValue -> Either String Ordering
+compareJSValues = undefined
 
-cmpFn :: Typed LValue -> Typed RValue -> Either String (JSValue -> Either String Ordering)
---cmpFn (Typed IntType _) (Typed _ (RString rstr)) = case safeRead rstr "integer" :: (Either String Int) of
-	--Right rstr' -> Right $ \lstr -> compare <$> safeRead lstr "integer" <*> return rstr'
-	--Left e -> Left e
-cmpFn (Typed IntType _) (Typed IntType (RString rstr)) = do
-	r <- safeRead rstr "integer" :: Either String Int
-	return cmp where
-		cmp (JSRational _ r') = Right $ compare r r'
-		cmp _ = Left $ "Type mismatch"
-	-- return $ \lstr -> compare <$> safeRead lstr "integer" <*> return rstr'
+extractJSValue :: TypeEnum -> KeyPath -> JSValue -> Either String JSValue
+extractJSValue = undefined
 
-
--- cmpFn StringType s = \s' -> Right $ compare s s'
--- cmpFn IntType s = let i = read s :: Int in p where
--- 		p (JSInteger i') = compare i i'
--- 		p _ = error "Type mismatch"
--- cmpFn IntType s = let i = read s :: Int in \s' -> Right $ compare i $ read s'
--- cmpFn FloatType s = let f = read s :: Float in \s' -> Right $ compare f $ read s'
--- cmpFn t s = error $ "Type mismatch: " ++ s ++ " is not a " ++ show t
-
+stringToJSValue :: TypeEnum -> String -> Either String JSValue
+stringToJSValue = undefined
